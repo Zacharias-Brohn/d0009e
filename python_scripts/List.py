c68d9b3416fe657e2a6b7331496326167f39127e
Zachbrolo <Zachbrolo@users.noreply.github.com>
2024-09-18 16:14:34 +0200

    python

---
+++ python_scripts/List.py
@@ -0,0 +1,21 @@
+lst = [1,2,3]
+lst1 = [6,7]
+lst2 = lst+lst1
+print(lst2)
+print(lst2[2:4])
+
+lst3 = ['a','b','c','d','e','f']
+print(lst3)
+lst3[1:3] = ['x','y']
+print(lst3)
+
+lst4 = ['Hello World!']
+splitlist = "l".split(lst4)
+print(split)
+
+words = "l".join(splitlist)
+print(words)
+
+letters = ", ".join(lst3)
+print(letters)
+
--- python_scripts/StrToIntForLoop.py
+++ python_scripts/StrToIntForLoop.py
@@ -1,7 +1,6 @@
 def strToInt(str):
-    i = 0
-    n = 0
     toInt = 0
+    n = 0
     for i in str:
         toInt += (ord(i) - ord('0')) * (10 ** ((len(str)-1)-n))
         n += 1
