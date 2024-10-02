c88401a750e177e1cb200f0ccf014c55f6a4ed40
Zachbrolo <officialalways88@gmail.com>
2024-10-02 17:32:21 +0200

    Comments

--- python_scripts/Lab3/Dictionary.py
+++ python_scripts/Lab3/Dictionary.py
@@ -1,4 +1,5 @@
-def runMain():
+def runMain(): # Vilken typ av Ordlista att kalla
+  print("=== Choose type ===")
   print("1: Listor")
   print("2: Tupel")
   print("3: Dictionary")
@@ -16,71 +17,71 @@ def runMain():
     print("Invalid input")
     runMain()
 
-def runMainLists():
-  wordList = []
+def print_menu(): # GUI meny
+  print("\n=== Main Menu ===")
+  print("1: Insert word")
+  print("2: Lookup word")
+  print("3: Exit")
+
+def runMainLists():     # Ordlista med listor
+  wordList = []         # Beskriva listorna
   descriptionList = []
-  while True:
+  while True:           # Kör programmet oändligt så listor inte återställs
     print_menu()
     try:
       choice = int(input("Input menu choice: "))
       if choice in range(1,4):
         handle_choiceLists(choice, wordList, descriptionList)
       else:
-        raise ValueError
-    except ValueError:
+        raise ValueError  # För att fånga input utanför 1-3 med nedan
+    except ValueError:    # För att fånga input som inte är integer
       print("Invalid input, try again.")
-
-def print_menu(): # GUI meny
-  print("=== Main Menu ===")
-  print("1: Insert word")
-  print("2: Lookup word")
-  print("3: Exit")
 
 def handle_choiceLists(choice, wordList, descriptionList):
   if choice == 1:
     word = str(input("Write the word you want to add: "))
     desc = str(input("\nWrite the description of the word: "))
     insertWordLists(word, desc, wordList, descriptionList)
-    return
   elif choice == 2:
     word = str(input("Write the word you want to lookup: "))
     lookupWordLists(word, wordList, descriptionList)
   elif choice == 3:
     print("Exiting...")
     exit()
+  return
 
 def insertWordLists(word, description, wordList, descriptionList):
-  wordList.append(word)
-  descriptionList.append(description)
+  wordList.append(word)               # Lägg till i Ordlista
+  descriptionList.append(description) # Lägg till i definitions lista
   print(f"Added '{word}' with description '{description}' to the dictionary.")
   return
 
 def lookupWordLists(word, wordList, descriptionList):
-  if word in wordList:
-    index = wordList.index(word)
+  if word in wordList:            # Kollar om ordet finns
+    index = wordList.index(word)  # Samma index på ordet som definition
     print(f"Description of '{word}': {descriptionList[index]}")
   else:
     print(f"'{word}' was not found in dictionary.")
   return
 
-def runMainTupel():
-  tupel = ()
+def runMainTupel(): # Samma upplägg men med tupel
+  tupel = ()        # Tilldela tupel till variabel
   while True:
     print_menu()
     try:
       choice = int(input("Input menu choice: "))
       if choice in range(1,4):
-        tupel = handle_choiceTupel(choice, tupel)
+        tupel = handle_choiceTupel(choice, tupel) # Måste tilldela till tupel
       else:
         raise ValueError
     except ValueError:
       print("Invalid input, try again.")
 
-def handle_choiceTupel(choice, tupel):
+def handle_choiceTupel(choice, tupel):  # Hantera val
   if choice == 1:
     word = str(input("Write the word you want to add: "))
     desc = str(input("Write the description of the word: "))
-    tupel = insertWordTupel(tupel, word, desc)
+    tupel = insertWordTupel(tupel, word, desc)  # Samma här, tilldela till tupel
   elif choice == 2:
     word = str(input("Write word you want to lookup: "))
     lookupWordTupel(tupel, word)
@@ -91,20 +92,19 @@ def handle_choiceTupel(choice, tupel):
 
 def insertWordTupel(tupel, word, desc):
   print(f"Added '{word}' with description '{desc}' to the dictionary.")
-  return tupel + (word,desc)
+  return tupel + (word,desc) # Returnera ord och definition
 
 def lookupWordTupel(tupel, word):
-  if word in tupel:
-    index = tupel.index(word)
-    print(f"Description of '{word}': {tupel[(index+1)]}")
+  if word in tupel:                                       # Kolla att ordet finns
+    index = tupel.index(word)                             # Index av ordet
+    print(f"Description of '{word}': {tupel[(index+1)]}") # Skriver index+1 för att få definition
   else:
     print(f"'{word}' was not found in dictionary.")
   return
 
 def runMainDictionary():
-  dictionary = {}
+  dictionary = {} # Tilldela variabel med dictionary
   while True:
-    print(dictionary)
     print_menu()
     try:
       choice = int(input("Input menu choice: "))
@@ -119,20 +119,20 @@ def handle_choiceDictionary(choice, dictionary):
   if choice == 1:
     word = input("Write the word you want to add: ")
     desc = input("Write the description of the word: ")
-    insertWordDictionary(dictionary, word, desc)  # Insert word into the dictionary
+    insertWordDictionary(dictionary, word, desc)
   elif choice == 2:
     word = input("Write the word you want to lookup: ")
-    lookupWordDictionary(dictionary, word)  # Lookup the word in the dictionary
+    lookupWordDictionary(dictionary, word)
   elif choice == 3:
     print("Exiting...")
     exit()
 
 def insertWordDictionary(dictionary, word, desc):
-  dictionary[word] = desc
+  dictionary[word] = desc # Lägg till ord och definition till Ordlista
   print(f"Added '{word}' with description '{desc}' to the dictionary.")
 
 def lookupWordDictionary(dictionary, word):
-  if word in dictionary:
+  if word in dictionary:  # Kolla om ordet finns
     print(f"Description of '{word}': {dictionary[word]}.")
   else:
     print(f"'{word}' was not found in the dictionary.")
