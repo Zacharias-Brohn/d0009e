4fa2d6549e1347ae3d04493d354c97f1c39956d7
Zacharias-Brohn <officialalways88@gmail.com>
2024-10-03 12:43:24 +0200

    changes

--- python_scripts/Lab3/Dictionary.py
+++ python_scripts/Lab3/Dictionary.py
@@ -4,7 +4,7 @@ def runMain(): # Vilken typ av Ordlista att kalla
   print("2: Tupel")
   print("3: Dictionary")
   try:
-    val = int(input("Val: "))
+    val = int(input("Val: \n"))
     if val == 1:
       runMainLists()
     elif val == 2:
@@ -29,9 +29,11 @@ def runMainLists():     # Ordlista med listor
   descriptionList = []
   while True:           # Kör programmet oändligt så listor inte återställs
     print_menu()
+    #print(wordList)
+    #print(descriptionList)
     try:
       choice = int(input("Input menu choice: "))
-      if choice in range(1,4):
+      if choice in range(1,5):
         handle_choiceLists(choice, wordList, descriptionList)
       else:
         raise ValueError  # För att fånga input utanför 1-3 med nedan
@@ -61,20 +63,20 @@ def insertWordLists(word, description, wordList, descriptionList):
   return
 
 def lookupWordLists(word, wordList, descriptionList):
-  if word in wordList:            # Kollar om ordet finns
+  try:            # Kollar om ordet finns
     index = wordList.index(word)  # Samma index på ordet som definition
     print(f"Description of '{word}': {descriptionList[index]}")
-  else:
+  except ValueError:
     print(f"'{word}' was not found in dictionary.")
-  return
+    return
 
 def removeWordLists(word, wordList, descriptionList):
-  if word in wordList:
+  try:
     index = wordList.index(word) # Index av samma anledning som lookup
     wordList.pop(index) # Pop tar bort ordet
     descriptionList.pop(index) # Och definitionen 
     print(f"Removed '{word}' with description {descriptionList[index]}")
-  else:
+  except ValueError:
     print(f"'{word}' was not found in dictionary.")
     return
   
@@ -82,9 +84,10 @@ def runMainTupel(): # Samma upplägg men med tupel
   tupel = ()        # Tilldela tupel till variabel
   while True:
     print_menu()
+    print(tupel)
     try:
       choice = int(input("Input menu choice: "))
-      if choice in range(1,4):
+      if choice in range(1,5):
         tupel = handle_choiceTupel(choice, tupel) # Måste tilldela till tupel
       else:
         raise ValueError
@@ -112,59 +115,78 @@ def insertWordTupel(tupel, word, desc):
   return tupel + (word,desc) # Returnera ord och definition
 
 def lookupWordTupel(tupel, word):
-  if word in tupel:                                       # Kolla att ordet finns
-    index = tupel.index(word)                             # Index av ordet
+  try:                          # Kolla att ordet finns
+    index = tupel.index(word)   # Index av ordet
     print(f"Description of '{word}': {tupel[(index+1)]}") # Skriver index+1 för att få definition
-  else:
+  except ValueError:
     print(f"'{word}' was not found in dictionary.")
-  return
+    return
 
 def removeWordTupel(tupel, word):
   if word in tupel:
     index = tupel.index(word)
-    listCnvt = tupel
-    print(listCnvt)
-    listCnvt.pop(index)
-    print(listCnvt)
-    listCnvt.pop(index)
-    return tupel
+    if index % 2:
+      print(f"'{word}' was not found in dictionary.")
+    else:
+      tupel = tupel[:index] + tupel[index+2:]
   else:
     print(f"'{word}' was not found in dictionary.")
-    return
+  return tupel
+  #try:
+  #  index = tupel.index(word)
+  #  listCnvt = list(tupel)  # Konvertera till lista
+  #  listCnvt.pop(index)     # Pop tar bort ordet
+  #  listCnvt.pop(index)     # Igen för beskrivningen tar ordets plats
+  #  tupel = tuple(listCnvt) # Tillbaka till tupel
+  #  return tupel
+  #except ValueError:        # Fångar om ordet inte finns
+  #  print(f"'{word}' was not found in dictionary.")
+  #  return
 
 def runMainDictionary():
   dictionary = {} # Tilldela variabel med dictionary
   while True:
     print_menu()
+    # print(dictionary)
     try:
       choice = int(input("Input menu choice: "))
-      if choice in range(1,4):
+      if choice in range(1,5):
         handle_choiceDictionary(choice, dictionary)
       else:
         raise ValueError
     except ValueError:
-      print("Invalid input, try again.")
+      print("Invalid input, try again.\n")
 
 def handle_choiceDictionary(choice, dictionary):
   if choice == 1:
-    word = input("Write the word you want to add: ")
-    desc = input("Write the description of the word: ")
+    word = str(input("Write the word you want to add: "))
+    desc = str(input("Write the description of the word: "))
     insertWordDictionary(dictionary, word, desc)
   elif choice == 2:
-    word = input("Write the word you want to lookup: ")
+    word = str(input("Write the word you want to lookup: "))
     lookupWordDictionary(dictionary, word)
   elif choice == 3:
-    print("Exiting...")
+    word = str(input("Write the word you want to remove: "))
+    removeWordDictionary(dictionary, word)
+  elif choice == 4:
+    print("Exiting...\n")
     exit()
 
 def insertWordDictionary(dictionary, word, desc):
   dictionary[word] = desc # Lägg till ord och definition till Ordlista
-  print(f"Added '{word}' with description '{desc}' to the dictionary.")
+  print(f"Added '{word}' with description '{desc}' to the dictionary.\n")
 
 def lookupWordDictionary(dictionary, word):
   if word in dictionary:  # Kolla om ordet finns
     print(f"Description of '{word}': {dictionary[word]}.")
   else:
-    print(f"'{word}' was not found in the dictionary.")
+    print(f"'{word}' was not found in the dictionary.\n")
+
+def removeWordDictionary(dictionary, word):
+  try:
+    dictionary.pop(word)
+  except KeyError:
+    print(f"'{word}' was not found in the dictionary.\n")
+    return
 
 runMain()
--- python_scripts/Seminars/Classes.py
+++
@@ -1,4 +0,0 @@
-class Time:
-    hours = 0
-    minutes = 0
-    seconds = 0
